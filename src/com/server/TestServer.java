a400223dca10d6d20beb43daff022524dc56d157
Juan-Alberto Hernández-Martínez <juan.alberto.hernandez.martinez@gmail.com>
2020-03-08 02:51:50 -0600

    Deleting unnecessary class files

--- src/com/server/TestServer.java
+++
@@ -1,9 +0,0 @@
-package com.server;
-
-public class TestServer {	
-        
-	public static void main(String[] args) {		    
-		MainServer myServer=new MainServer();
-                myServer.startRunning();
-	}
-}
