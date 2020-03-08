345277a04df130b740b0a187ba868d4d7402528f
Juan-Alberto Hernández-Martínez <juan.alberto.hernandez.martinez@gmail.com>
2020-03-08 02:51:09 -0600

    Deleting unnecessary class files

--- src/com/client/TestClient.java
+++
@@ -1,9 +0,0 @@
-package com.client;
-
-public class TestClient {
-
-    public static void main(String[] args) {
-        Client client=new Client("127.0.0.1");
-        client.startRunning();
-    }
-}
